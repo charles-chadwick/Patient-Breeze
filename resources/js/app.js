caa42b4d772275e8529ed02cb2fbf4723ccc4926
Charles Chadwick <charles@mutuallyassured.com>
2025-10-24 22:01:12 -0700

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,80 @@
+import './bootstrap.js';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import {definePreset} from "@primeuix/themes";
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{purple.50}',
+            100: '{purple.100}',
+            200: '{purple.200}',
+            300: '{purple.300}',
+            400: '{purple.400}',
+            500: '{purple.500}',
+            600: '{purple.600}',
+            700: '{purple.700}',
+            800: '{purple.800}',
+            900: '{purple.900}',
+            950: '{purple.950}'
+        },
+        colorScheme: {
+            light: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{stone.50}',
+                    100: '{stone.100}',
+                    200: '{stone.200}',
+                    300: '{stone.300}',
+                    400: '{stone.400}',
+                    500: '{stone.500}',
+                    600: '{stone.600}',
+                    700: '{stone.700}',
+                    800: '{stone.800}',
+                    900: '{stone.900}',
+                    950: '{stone.950}'
+                }
+            },
+            dark: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{stone.50}',
+                    100: '{stone.100}',
+                    200: '{stone.200}',
+                    300: '{stone.300}',
+                    400: '{stone.400}',
+                    500: '{stone.500}',
+                    600: '{stone.600}',
+                    700: '{stone.700}',
+                    800: '{stone.800}',
+                    900: '{stone.900}',
+                    950: '{stone.950}'
+                }
+            }
+        }
+    }
+});
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./pages/**/*.vue', { eager: true })
+        return pages[`./pages/${name}.vue`]
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(PrimeVue, {
+                theme: {
+                    preset: MyPreset,
+                    options: {
+                        prefix: 'p',
+                        darkModeSelector: false,
+                        cssLayer: false
+                    }
+                }
+            })
+            .mount(el)
+    },
+})
