cd68bf3bb9d9801e430933890013abc6fde7c9f7
Charles Chadwick <charles@mutuallyassured.com>
2025-10-26 09:57:08 -0700

    Cleanup and adding middle name

--- resources/js/Pages/Patients/Show.vue
+++ resources/js/Pages/Patients/Show.vue
@@ -8,12 +8,13 @@ import { ref } from "vue";
 import Status from "./Partials/Status.vue";
 
 const props = defineProps ( { patient: Object } )
+const patient = props.patient.data; 
 const form = useForm ( {
   avatar: null
 } )
 
 function uploadAvatar () {
-  form.post ( PatientController.uploadAvatar ( props.patient.data.id ) )
+  form.post ( PatientController.uploadAvatar ( props.patient.id ) )
 }
 
 const showDialog = ref ( false )
@@ -36,27 +37,27 @@ const handleCloseDialog = () => {
       <template #content>
         <div class="flex justify-between items-start">
           <div>
-            <h1 class="p-card-title">{{ patient.data.attributes.full_name }}'s Profile</h1>
-            <p><span class="font-bold">Name:</span> {{ patient.data.attributes.full_name }}</p>
-            <p><span class="font-bold">DOB:</span> {{ patient.data.attributes.dob }}</p>
-            <p><span class="font-bold">Gender:</span> {{ patient.data.attributes.gender }}
-              <span v-if="patient.data.attributes.gender_identity !== ''">
-                / {{ patient.data.attributes.gender_identity }}
+            <h1 class="p-card-title">{{ patient.attributes.full_name }}'s Profile</h1>
+            <p><span class="font-bold">Name:</span> {{ patient.attributes.first_name }} {{ patient.attributes.middle_name}} {{ patient.attributes.last_name }}</p>
+            <p><span class="font-bold">DOB:</span> {{ patient.attributes.dob }}</p>
+            <p><span class="font-bold">Gender:</span> {{ patient.attributes.gender }}
+              <span v-if="patient.attributes.gender_identity !== ''">
+                / {{ patient.attributes.gender_identity }}
               </span>
             </p>
-            <p><span class="font-bold">Status:</span> <Status :status="patient.data.attributes.status" /></p>
-            <p><span class="font-bold">Created:</span> {{ patient.data.attributes.created_at }}</p>
+            <p><span class="font-bold">Status:</span> <Status :status="patient.attributes.status" /></p>
+            <p><span class="font-bold">Created:</span> {{ patient.attributes.created_at }}</p>
           </div>
           <div>
-            <div v-if="patient.data.attributes.avatar">
+            <div v-if="patient.attributes.avatar">
               <img
                   @click="handleShowDialog"
                   alt="Avatar"
                   class="rounded-2xl size-[128px] mx-auto"
-                  :src="patient.data.attributes.avatar"
+                  :src="patient.attributes.avatar"
               />
               <Link
-                  :href="PatientController.removeAvatar('patients.avatar.remove', patient.data.id)"
+                  :href="PatientController.removeAvatar('patients.avatar.remove', patient.id)"
                   method="post"
                   as="p"
                   class="flex-none cursor-pointer"
@@ -98,7 +99,7 @@ const handleCloseDialog = () => {
     <template #container>
       <img
           @click="handleCloseDialog"
-          :src="patient.data.attributes.avatar ?? '/avatars/default.jpg'"
+          :src="patient.attributes.avatar ?? '/avatars/default.jpg'"
           class="rounded-xl border-2 border-darker-300 hover:border-primary-600"
           alt="Avatar"
       />
