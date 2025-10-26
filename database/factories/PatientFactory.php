f621de162d7d8f7d601314a3b188b3bde8027f38
Charles Chadwick <charles@mutuallyassured.com>
2025-10-26 09:36:32 -0700

    Updating to have created_by_id

--- database/factories/PatientFactory.php
+++ database/factories/PatientFactory.php
@@ -3,7 +3,9 @@
 namespace Database\Factories;
 
 use App\Enums\Gender;
+use App\Enums\PatientStatus;
 use App\Models\Patient;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 
@@ -11,24 +13,25 @@ class PatientFactory extends Factory
 {
     protected $model = Patient::class;
 
-    public function definition(): array
+    public function definition() : array
     {
         return [
-            'status' => $this->faker->word(),
-
-            'prefix' => $this->faker->word(),
-            'first_name' => $this->faker->firstName(),
-            'middle_name' => $this->faker->firstName(),
-            'last_name' => $this->faker->lastName(),
-            'suffix' => $this->faker->word(),
-            'dob' => Carbon::now(),
-            'gender' => $this->faker->randomElement(Gender::cases()),
+            'status'          => $this->faker->randomElement(PatientStatus::cases()),
+            'prefix'          => $this->faker->word(),
+            'first_name'      => $this->faker->firstName(),
+            'middle_name'     => $this->faker->firstName(),
+            'last_name'       => $this->faker->lastName(),
+            'suffix'          => $this->faker->word(),
+            'dob'             => Carbon::now(),
+            'gender'          => $this->faker->randomElement(Gender::cases()),
             'gender_identity' => $this->faker->word(),
-            'email' => $this->faker->unique()
+            'email'           => $this->faker->unique()
                 ->safeEmail(),
-            'password' => bcrypt('password'),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
+            'password'        => bcrypt('password'),
+            'created_at'      => Carbon::now(),
+            'updated_at'      => Carbon::now(),
+            'created_by_id'   => User::inRandomOrder()
+                ->first()->id,
         ];
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,12 +3,13 @@
 namespace Database\Factories;
 
 use App\Enums\UserRole;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
@@ -47,6 +48,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'created_by_id' => 1
         ];
     }
 
