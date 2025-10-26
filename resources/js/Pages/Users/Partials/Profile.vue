db42c7d54a87a8fc12063c68a92b95c53b30f73c
Charles Chadwick <charles@mutuallyassured.com>
2025-10-26 14:23:33 -0700

    Adding size to the profile.
    I really should move the avatar itself into a separate component.

--- resources/js/Pages/Users/Partials/Profile.vue
+++ resources/js/Pages/Users/Partials/Profile.vue
@@ -2,20 +2,36 @@
 <script setup>
 import { Link } from "@inertiajs/vue3";
 import UserController from "../../../actions/App/Http/Controllers/UserController";
+
+import { Dialog } from "primevue";
 import { ref, computed } from "vue";
 
 const props = defineProps ( {
   user: Object,
   show_avatar: { type: Boolean, default: true },
-  show_large_avatar: { type: Boolean, default: true },
+  avatar_size: { type: String, default: 'md'},
+  show_large_avatar: { type: Boolean, default: false },
   show_name: { type: Boolean, default: true },
 } )
+
+const size = computed (() => {
+  const sizes = {
+    'xs': 'w-6 h-6',
+    'sm': 'w-8 h-8',
+    'md': 'w-12 h-12',
+    'lg': 'w-16 h-16',
+    'xl': 'w-20 h-20'
+  }
+  return sizes[ props.avatar_size ] || sizes[ 'md' ]
+});
 
 const user = computed ( () => props.user )
 const showDialog = ref ( false )
 
 const handleShowDialog = () => {
-  showDialog.value = true;
+  if (props.show_large_avatar) {
+    showDialog.value = true;
+  }
 }
 
 const handleCloseDialog = () => {
@@ -38,8 +54,9 @@ const handleCloseDialog = () => {
           @click="handleShowDialog"
           :src="user.attributes.avatar"
           alt="Avatar"
-          class="w-12 h-12 rounded-lg border-primary-300 border-2"
-      >
+          v-tooltip.bottom="{value: user.attributes.full_name}"
+          :class="[size, 'rounded-2xl border-darker-300 hover:border-primary-400 border-2 saturate-50 hover:saturate-100']"
+      />
     </div>
   </div>
   <Dialog
@@ -50,8 +67,8 @@ const handleCloseDialog = () => {
     <template #container>
       <img
           @click="handleCloseDialog"
-          :src="user.data.attributes.avatar ?? '/avatars/default.jpg'"
-          class="rounded-xl border-2 border-darker-300 hover:border-primary-600"
+          :src="user.attributes.avatar ?? '/avatars/default.jpg'"
+          class="rounded-xl border-2 border-darker-300 hover:border-primary-400 "
           alt="Avatar"
       />
     </template>
