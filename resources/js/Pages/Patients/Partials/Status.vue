775d720de11fe4736a8a3b958cd529d40e27f3e0
Charles Chadwick <charles@mutuallyassured.com>
2025-10-26 09:28:54 -0700

    Decided to create my own Badge

--- resources/js/Pages/Patients/Partials/Status.vue
+++ resources/js/Pages/Patients/Partials/Status.vue
@@ -1,26 +1,32 @@
 <script setup>
-import { Badge } from 'primevue';
 import { computed } from "vue";
 
-const props = defineProps ( { status: String, size: { type: String, default: 'small' } } )
+const props = defineProps ( { status: String, size: { type: String, default: 'normal' } } )
 const status = computed ( () => props.status )
+const size = computed ( () => props.size )
 
 const severity = {
-  Active: '',
-  Inactive: 'secondary',
-  Deceased: 'danger',
-  Prospective: 'warn'
+  Active: 'fill-accent-400',
+  Inactive: 'fill-darker-400',
+  Deceased: 'fill-red-400',
+  Prospective: 'fill-yellow-400',
+}
+
+const sizes = {
+  small: 'size-1.5',
+  normal: 'size-3',
+  large: 'size-5',
 }
 
 </script>
 
 <template>
-  <Badge
-      :size="size"
-      :severity="severity[status]"
-  >
+  <span class="inline-flex items-center gap-x-1.5 rounded-md py-1 text-sm font-medium inset-ring inset-ring-white/10">
+    <svg :class="sizes[size] + ` ` + severity[status]" viewBox="0 0 6 6" aria-hidden="true">
+      <circle cx="3" cy="3" r="3" />
+    </svg>
     {{ status }}
-  </Badge>
+  </span>
 </template>
 
 <style scoped>
