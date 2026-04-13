b52a1b03624364ca6231002c74a9411b5c4a0304
Charles Chadwick <charles.chadwick@3xcorp.com>
2026-04-13 09:10:09 -0700

    Refactor factories and seeders: split name into `first_name` and `last_name`, add prefixes, suffixes, and improved date handling.

--- database/factories/PatientFactory.php
+++ database/factories/PatientFactory.php
@@ -2,10 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Enums\GenderAtBirth;
 use App\Models\Patient;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Carbon;
 
 class PatientFactory extends Factory
 {
@@ -13,15 +13,23 @@ class PatientFactory extends Factory
 
     public function definition(): array
     {
+        $gender_at_birth = $this->faker->randomElement(GenderAtBirth::cases());
+
+        $prefix = match ($gender_at_birth) {
+            GenderAtBirth::Male => 'Mr.',
+            GenderAtBirth::Female => $this->faker->randomElement(['Mrs.', 'Ms.']),
+            GenderAtBirth::Unknown => $this->faker->randomElement(['Mr.', 'Mrs.', 'Ms.']),
+        };
+
+        $created_at = fake()->dateTimeBetween('2021-01-01', 'yesterday');
+
         return [
-            'date_of_birth' => Carbon::now(),
-
-            'gender_at_birth' => $this->faker->word(),
-            'gender_identity' => $this->faker->word(),
-            'blood_type' => $this->faker->word(),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-
+            'prefix' => $prefix,
+            'date_of_birth' => fake()->dateTimeBetween('-100 years', '-18 months'),
+            'gender_at_birth' => $gender_at_birth,
+            'blood_type' => $this->faker->randomElement(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']),
+            'created_at' => $created_at,
+            'updated_at' => $created_at,
             'user_id' => User::factory(),
         ];
     }
