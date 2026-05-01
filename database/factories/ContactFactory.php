9fc34859b42e2040ce2012d0862bee0ebef35be3
Charles <charles@mutuallyassured.com>
2026-04-30 17:42:34 -0700

    feat: add ContactFactory

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\ContactType;
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Contact>
+ */
+class ContactFactory extends Factory
+{
+    protected $model = Contact::class;
+
+    public function definition(): array
+    {
+        return [
+            'type' => fake()->randomElement(ContactType::cases()),
+            'phone' => fake()->optional()->phoneNumber(),
+            'street_address' => fake()->optional()->streetAddress(),
+        ];
+    }
+}
