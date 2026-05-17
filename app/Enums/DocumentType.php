968702bb02254ad7600749ee6733edf19f895226
Charles <charles@mutuallyassured.com>
2026-05-17 13:59:20 -0700

    feat: add DocumentType enum

---
+++ app/Enums/DocumentType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Enums;
+
+enum DocumentType: string
+{
+    case LabResult = 'LabResult';
+    case Insurance = 'Insurance';
+    case Referral = 'Referral';
+    case Consent = 'Consent';
+    case Prescription = 'Prescription';
+    case Identification = 'Identification';
+    case Certification = 'Certification';
+    case Note = 'Note';
+    case Other = 'Other';
+
+    /** @return string[] */
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
