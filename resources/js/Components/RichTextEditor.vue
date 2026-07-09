5711dbadee8f68fcbca69410fcad2bb13cfdb5e2
Charles <charles@mutuallyassured.com>
2026-07-08 17:23:09 -0700

    feat: add Quill dependency and RichTextEditor wrapper

---
+++ resources/js/Components/RichTextEditor.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { onBeforeUnmount, onMounted, ref, watch } from 'vue'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+        default: '',
+    },
+    placeholder: {
+        type: String,
+        default: '',
+    },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const editor_element = ref(null)
+let quill = null
+let is_internal_change = false
+
+const toolbar_options = [
+    ['bold', 'italic', 'underline'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['link'],
+    ['clean'],
+]
+
+onMounted(() => {
+    quill = new Quill(editor_element.value, {
+        theme: 'snow',
+        placeholder: props.placeholder,
+        modules: { toolbar: toolbar_options },
+    })
+
+    if (props.modelValue) {
+        quill.clipboard.dangerouslyPasteHTML(props.modelValue)
+    }
+
+    quill.on('text-change', () => {
+        is_internal_change = true
+        const html = quill.getLength() > 1 ? quill.root.innerHTML : ''
+        emit('update:modelValue', html)
+    })
+})
+
+watch(() => props.modelValue, (value) => {
+    if (is_internal_change) {
+        is_internal_change = false
+        return
+    }
+    if (quill && value !== quill.root.innerHTML) {
+        quill.clipboard.dangerouslyPasteHTML(value || '')
+    }
+})
+
+onBeforeUnmount(() => {
+    quill = null
+})
+</script>
+
+<template>
+    <div class="rounded-lg border border-border bg-white">
+        <div ref="editor_element"></div>
+    </div>
+</template>
